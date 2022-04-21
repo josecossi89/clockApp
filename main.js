16e5b660d2cf708258130ddcea32e7b6521ed213
Jose Cossi <josecossib@gmail.com>
2022-04-21 03:07:21 -0300

    setIntervals added

--- main.js
+++ main.js
@@ -1,24 +1,34 @@
-let hours = document.getElementById("hours");
-let minutes = document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
-let ampm = document.getElementById("ampm");
+setInterval(() => {
+  let hours = document.getElementById("hours");
+  let minutes = document.getElementById("minutes");
+  let seconds = document.getElementById("seconds");
+  let ampm = document.getElementById("ampm");
 
-let h = new Date().getHours();
-let m = new Date().getMinutes();
-let s = new Date().getSeconds();
-let am = h > 12 ? "PM" : "AM";
+  let hh = document.getElementById("hh");
+  let mm = document.getElementById("mm");
+  let ss = document.getElementById("ss");
 
-//Convert 24hrs clock to 12hrs clock
-if (h > 12) {
-  h = h - 12;
-}
+  let h = new Date().getHours();
+  let m = new Date().getMinutes();
+  let s = new Date().getSeconds();
+  let am = h > 12 ? "PM" : "AM";
 
-//Add zero before single digit number
-h = h < 10 ? "0" + h : h;
-m = m < 10 ? "0" + m : m;
-s = s < 10 ? "0" + s : h;
+  //Convert 24hrs clock to 12hrs clock
+  if (h > 12) {
+    h = h - 12;
+  }
 
-hours.innerHTML = h;
-minutes.innerHTML = m;
-seconds.innerHTML = s;
-ampm.innerHTML = am;
+  //Add zero before single digit number
+  h = h < 10 ? "0" + h : h;
+  m = m < 10 ? "0" + m : m;
+  s = s < 10 ? "0" + s : h;
+
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  ampm.innerHTML = am;
+
+  hh.style.strokeDashoffset = 440 - (440 * h) / 12;
+  mm.style.strokeDashoffset = 440 - (440 * m) / 60;
+  ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+});
