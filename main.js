0aba4fae2a6403130f9043f38af10f7a39b4ab6e
Jose Cossi <josecossib@gmail.com>
2022-04-23 01:28:25 -0300

    functions finished

--- main.js
+++ main.js
@@ -8,6 +8,10 @@ setInterval(() => {
   let mm = document.getElementById("mm");
   let ss = document.getElementById("ss");
 
+  let hr_dot = document.querySelector(".hr_dot");
+  let min_dot = document.querySelector(".min_dot");
+  let sec_dot = document.querySelector(".sec_dot");
+
   let h = new Date().getHours();
   let m = new Date().getMinutes();
   let s = new Date().getSeconds();
@@ -21,14 +25,18 @@ setInterval(() => {
   //Add zero before single digit number
   h = h < 10 ? "0" + h : h;
   m = m < 10 ? "0" + m : m;
-  s = s < 10 ? "0" + s : h;
+  s = s < 10 ? "0" + s : s;
 
-  hours.innerHTML = h;
-  minutes.innerHTML = m;
-  seconds.innerHTML = s;
+  hours.innerHTML = h + "<br><span>Hours</span>";
+  minutes.innerHTML = m + "<br><span>Minutes</span>";
+  seconds.innerHTML = s + "<br><span>Seconds</span>";
   ampm.innerHTML = am;
 
   hh.style.strokeDashoffset = 440 - (440 * h) / 12;
   mm.style.strokeDashoffset = 440 - (440 * m) / 60;
   ss.style.strokeDashoffset = 440 - (440 * s) / 60;
+
+  hr_dot.style.transform = `rotate(${h * 30}deg)`;
+  min_dot.style.transform = `rotate(${m * 6}deg)`;
+  sec_dot.style.transform = `rotate(${s * 6}deg)`;
 });
